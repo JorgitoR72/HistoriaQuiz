091299d9012b1eda8d49f3d4e1d629b5bc4d0f2c
JorgitoR72 <joreju@floridauniversitaria.es>
2024-04-16 15:00:33 +0200

    Update_composer

--- AppEnd/src/Entity/User.php
+++ AppEnd/src/Entity/User.php
@@ -23,6 +23,9 @@ class User
     #[ORM\Column(length: 255)]
     private ?string $mail = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $role = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -60,6 +63,18 @@ class User
     public function setMail(string $mail): static
     {
         $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): static
+    {
+        $this->role = $role;
 
         return $this;
     }
